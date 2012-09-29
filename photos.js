7d2a6f2b8f34e0c5b666b9443bf0fdf1a9392cb0
ishmael <ishmael@blackbalto.com>
2012-09-29 17:01:51 +0100

    change be more like jquery plugin

--- photos.js
+++ photos.js
@@ -1,6 +1,6 @@
 /*global jQuery*/
 
-var setupPhotos = (function ($) {
+(function ($) {
     function loadAllPhotos (tags, max, callback) {   
   		var photos = [];
   	  var requests =  $.map(tags, function(tag){
@@ -34,32 +34,28 @@ var setupPhotos = (function ($) {
 
     function imageAppender (img) {
   		var fileId = img.src.substring(img.src.lastIndexOf('/')+1,img.src.lastIndexOf('.'));
-  		var elm = document.createElement('div');
-  		elm.className = 'photo';
-  		var icon = document.createElement('div');
-  		icon.id = fileId;
-  		icon.className = "icon-large " + ((localStorage.getItem(fileId)=='true') ? "icon-heart" :	"icon-heart-empty");
-  		elm.appendChild(img);
-  		elm.appendChild(icon);
-  		var holder = document.getElementById('photos');
-  		holder.appendChild(elm);
-		  
 		  var favoriteClick =  function(event){
   		  event.preventDefault();
   		  localStorage.setItem(this.id,!(localStorage.getItem(this.id)=='true'));
   		  $(this).toggleClass('icon-heart icon-heart-empty');
   		  return false;
   		};
+  		var $photo = $('<div/>').addClass('photo');
+  		var $icon = $('<div/>')
+  		  .attr('id',fileId)
+        .addClass("icon-large " + ((localStorage.getItem(fileId)=='true') ? "icon-heart" :	"icon-heart-empty"))
+  			.on('click',favoriteClick);
   		
-  		$(icon).on('click',favoriteClick);
+  		$photo.append(img,$icon);
+  		this.append($photo);
   		
-  		return elm;
+  		return $photo;
     }
 
     // ----
     
     var max_per_tag = 5;
-    return function setup (tags, callback) {
+    $.fn.flickrPhotos = function(tags, callback) {
 		  var renderItem = function (items,err) {
         if(err && callback){ 
   				return callback(err); 
@@ -69,14 +65,14 @@ var setupPhotos = (function ($) {
             var deferred = $.Deferred();
             var img = new Image();
             img.onload = function(){
-               var photoElement = imageAppender(img);
+               var photoElement = imageAppender.call(this,img);
                photoElements.push(photoElement);
                deferred.resolve(photoElement);
-            }
-            img.onerror = deferred.reject();
+            }.bind(this);
+            img.onerror = deferred.reject;
             img.src = photo;
             return deferred.promise();
-        };
+        }.bind(this);
             
         $.when.apply(null,items.map(renderPhoto))
   			  .done(function(){
@@ -84,8 +80,9 @@ var setupPhotos = (function ($) {
         				  callback(photoElements);
         		  }
     			});
-      };
+      }.bind(this);
 		
       loadAllPhotos(tags, max_per_tag, renderItem);
+  		return this;
     };
 }(jQuery));
